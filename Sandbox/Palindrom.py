617f3d45caae8009e430b5366ce403027a5997d6
Нұр <78252057+Noorius@users.noreply.github.com>
2021-02-09 23:20:01 +0600

    Add files via upload

---
+++ Sandbox/Palindrom.py
@@ -0,0 +1,9 @@
+def palindrom(word):
+    from collections import deque
+    dq=deque(word)
+    while len(dq)>1:
+        if dq.popleft() != dq.pop():
+            return False
+    return True
+
+print(palindrom(input()))
